8d536bcebc0aee212c3dde9f9dcc759e0d124ecf
MateoTeo <mateuszbeltowski98@gmail.com>
2024-02-04 11:26:30 +0100

    Kolejne zajecia

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -13,10 +13,33 @@ public class Main {
         driver.get("file:///C:/Users/ACER/Desktop/styczen/index.html");
         System.out.println("adrs strony to " +  driver.getCurrentUrl());
         System.out.println(driver.getTitle());
-        WebElement pierwszyParagraf = driver.findElement(By.tagName("p"));
-        String zawartosc = pierwszyParagraf.getText();
+        WebElement paragraf = driver.findElement(By.tagName("p"));
+        // String zawartosc = pierwszyParagraf.getText();
+        // System.out.println(zawartosc);
 
-        System.out.println(zawartosc);
+        List<WebElement> listaP = driver.findElements(By.tagName("p")); // znalezienie wszystkich paragrafów
+        for (WebElement i:listaP) {
+            System.out.println("element listy: " + i.getText());
+        }
+
+        WebElement pierwszyParagraf = driver.findElement(By.name("pierwszyParagraf"));
+        System.out.println("Odnajdywanie po atrybucie name: " + pierwszyParagraf.getText());
+
+        WebElement drugiParagraf = driver.findElement(By.id("drugiParagraf"));
+        System.out.println("Odnajdywanie po atrybucie id: " + drugiParagraf.getText());
+
+        WebElement trzeciParagraf = driver.findElement(By.className("new"));
+        System.out.println("Odnajdywanie po atrybucie klasie: " + trzeciParagraf.getText());
+
+        // xpath
+        WebElement poXpath = driver.findElement(By.xpath("/html/body/h4"));
+        System.out.println(poXpath.getText());
+
+        WebElement podXpathDwa = driver.findElement(By.xpath("//h5[@class='xpath']"));
+        System.out.println(podXpathDwa.getText());
+
+
+
         driver.quit();
 
     }
